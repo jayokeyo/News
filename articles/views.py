8d368c31d938d0883b7cb393973fa1aa574ee5b2
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-17 16:04:18 +0300

    Updated multiple files

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,43 @@
 # articles/views.py
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
 from .models import Article
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin,ListView):
     model = Article
     template_name = "article_list.html"
+
+class ArticleDetailView(LoginRequiredMixin,DetailView):
+    model = Article
+    template_name = "article_detail.html"
+
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Article
+    fields = ("title","body",)
+    template_name = "article_edit.html"
+    success_url = reverse_lazy("article_list")
+
+    def test_func(self): # new
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class ArticleCreateView(LoginRequiredMixin,CreateView):
+    model = Article
+    fields = ("title","body",)
+    template_name = "article_new.html"
+    success_url = reverse_lazy("article_list")
+
+    def form_valid(self, form): # new
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Article
+    template_name = "article_delete.html"
+    success_url = reverse_lazy("article_list")
+
+    def test_func(self): # new
+        obj = self.get_object()
+        return obj.author == self.request.user
